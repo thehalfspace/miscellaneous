c253827467366994ababd4dbd0f749a1e35d6b8e
Prithvi Thakur <prithvi.thakur93@gmail.com>
2018-07-25 16:47:58 -0400

    add rate and state solver

---
+++ rate_state_friction.py
@@ -0,0 +1,39 @@
+# Quasi-dynamic 1D earthquake model: rate and state friction solver
+
+
+import numpy as np
+from scipy.optimize import fsolve
+from scipy.integrate import ode, odeint
+import matplotlib.pyplot as plt
+
+# Global constants
+class Parameters:
+    mu = 3e10        # Shear Modulus (Pa)
+    density = 2700
+    vs = np.sqrt(mu/density)
+    eta = mu/(2*vs)  # radiation damping coefficient
+
+    L = 60*1000      # width of plate boundary (m)
+    k = mu/L         # stiffness aka spring constant
+
+    Vpl = 1e-9
+
+    Seff = 50e6     # effective normal stress (Pa)
+    a = 0.015
+    b = 0.02
+    Dc = 0.2
+    fo = 0.6
+    Vo = 1e-6
+
+class RSF:
+
+    def F(V, Seff, state):
+        f = a * np.arcsinh(V/(2*Vo)*np.exp(state/a))
+
+        return f*Seff
+
+    def G(V, state):
+        return (b*Vo/Dc)*(np.exp((fo - state)/b) - (V - Vo))
+
+
+
